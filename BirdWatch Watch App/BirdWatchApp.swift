608f8cd62f94b8efb06c2155bd7e2f258c1b5db4
Ryan Brunk <rdabrunk@gmail.com>
2026-05-19 13:49:50 -0400

    Initial Commit

---
+++ BirdWatch Watch App/BirdWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdWatchApp.swift
+//  BirdWatch Watch App
+//
+//  Created by Ryan Brunk on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct BirdWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirdWatch Watch AppTests/BirdWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BirdWatch_Watch_AppTests.swift
+//  BirdWatch Watch AppTests
+//
+//  Created by Ryan Brunk on 5/19/26.
+//
+
+import Testing
+@testable import BirdWatch_Watch_App
+
+struct BirdWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
